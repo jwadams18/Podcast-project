90dc05d785be1250da8828b332d173ea046deb53
James Adams <jadams18@austincollege.edu>
2020-10-28 23:38:12 -0500

    Created sample data, basic getters and setters until back-end library implemented

--- src/Main/Model.java
+++ src/Main/Model.java
@@ -2,20 +2,53 @@ package Main;
 
 import com.apptastic.rssreader.Item;
 import com.apptastic.rssreader.RssReader;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public class Model {
 
-    private RssReader reader;
+    private FXMLLoader mainLoader, secondaryLoader;
+    private secondaryController test;
 
     public Model(){
-        reader = new RssReader();
+
     }
 
-    public Stream<Item> read(String url) throws IOException {
-        return reader.read(url);
+    public static void fillSampleData(ObservableList<Podcast> backingList) {
+        backingList.add(new Podcast("Name 1", "Author 1", 1,1, new JTextArea("Welcome to the new season!"), "resources/playIcon.png"));
+        backingList.add(new Podcast("Name 2", "Author 2", 1,2, new JTextArea("Episode one was much better!"), "resources/playIcon.png"));
+        backingList.add(new Podcast("Name 3", "Author 3", 1,3, new JTextArea("When is the new season coming out?"), "resources/playIcon.png"));
+        backingList.add(new Podcast("Name 4", "Author 4", 1,4, new JTextArea("LAST EPISODE!"), "resources/playIcon.png"));
+        backingList.add(new Podcast("Name 5", "Author 5", 2,1, new JTextArea("SEASON 2!!"), "resources/playIcon.png"));
+    }
+
+    public void setMainController(FXMLLoader fxmlCtr){
+        this.mainLoader = fxmlCtr;
+    }
+
+    public FXMLLoader getMainController(){
+        return this.mainLoader;
+    }
+
+    public void setAddWindowController(FXMLLoader fxmlCntr){
+        this.secondaryLoader = fxmlCntr;
+    }
+
+    public FXMLLoader getAddWindowController(){
+        return secondaryLoader;
+    }
+
+    public void setSecondaryController(secondaryController tst){
+        this.test = tst;
+    }
+
+    public secondaryController getSecondaryController(){
+        return this.test;
     }
 
 
