d16cf14d11aaf6b0ba094d2eea9df7c8840e10cd
James Adams <jadams18@austincollege.edu>
2020-10-29 10:49:24 -0500

    Optimized naming and imports

--- src/Main/secondaryController.java
+++ src/Main/secondaryController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
