cf3dfdb06f56ae598327a18949bd1d359cba038f
James Adams <jadams18@austincollege.edu>
2020-11-03 10:55:13 -0600

    Implementing all features to display info of Podcast objects.

--- src/Main/PodcastCell.java
+++ src/Main/PodcastCell.java
@@ -1,14 +1,11 @@
 package Main;
 
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
@@ -16,7 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PodcastCell extends ListCell<Podcast> implements Initializable {
+public class PodcastCell extends ListCell<Podcast>{
 
     @FXML
     private HBox container;
@@ -40,44 +37,48 @@ public class PodcastCell extends ListCell<Podcast> implements Initializable {
 
     public void loadFXML(){
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("PodcastCell.fxml"));
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("PodcastCell.fxml"));
             loader.setController(this);
-            loader.setRoot(this);
+//            loader.setRoot(this);
             loader.load();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         System.out.println("Podcastcell.fxml loaded");
-    }
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 
     @Override
     protected void updateItem(Podcast podcast, boolean empty) {
         super.updateItem(podcast, empty);
 
+//        isPlaying.setVisible(false);
+//        hasNotes.setVisible(false);
+
         if(empty || podcast == null) {
             podcastTitle.setVisible(false);
             podcastProgress.setVisible(false);
             setText(null);
-            setGraphic(null);
+            HBox temp = new HBox();
+            temp.setPrefHeight(34.0);
+            setGraphic(temp);
         } else {
             podcastTitle.setVisible(true);
             podcastTitle.setText(podcast.getTitle());
             podcastProgress.setProgress(podcast.getProgress());
             podcastProgress.setVisible(true);
 
-
             if(podcast.hasNotes()){
+                System.out.println("Is this running?");
                 hasNotes.setVisible(true);
             }
             if(podcast.isPlaying()){
-//                isPlaying.setVisible(true);
+                isPlaying.setVisible(true);
             }
+
+            setGraphic(container);
         }
-        setGraphic(container);
-        setContentDisplay(ContentDisplay.CENTER);
+
+//        setContentDisplay(ContentDisplay.CENTER);
     }
 }
