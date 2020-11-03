e8285739d5eb38e5ac91947648b8f3c8c214da44
James Adams <jadams18@austincollege.edu>
2020-11-03 10:53:27 -0600

    Updated title to NoteCast!

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("NoteCast!");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
