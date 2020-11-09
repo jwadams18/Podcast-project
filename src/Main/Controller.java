eec91886359f793b1b9ba4fc80363ea27521b65f
James Adams <jadams18@austincollege.edu>
2020-11-09 15:38:04 -0600

    Implemented real data loading instead of filler data. Fixed image loading for podcast cover icon

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -14,20 +14,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -89,24 +83,22 @@ public class Controller implements Initializable {
     private TextArea noteArea;
 
 
-    //TODO IDEA: change search into Library where all podcast are listed
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         //Loads the fxml files for sample scene
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("sample.fxml"));
-        m.setMainController(fxmlLoader);
+        m.setMainLoader(fxmlLoader);
 
         //Sets the "factory" that will make each new listView cell, set selection to single
         queueView.setCellFactory(new PodcastCellFactory());
         queueView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
         //Fills will sample data from Model, will be replaced when back-end loading is finished
-        Model.fillSampleData(queueList);
+//        Model.fillSampleData(queueList);
         //Sorts list based on comparator defined below
-        SortedList<Podcast> sortedList = new SortedList<>(queueList);
+        SortedList<Podcast> sortedList = new SortedList<>(m.getQueueList());
 
         sortedList.setComparator((Podcast1, Podcast2) -> {
             int result = Podcast1.getAuthor().compareToIgnoreCase(Podcast2.getAuthor());
@@ -129,7 +121,12 @@ public class Controller implements Initializable {
                     noteTitle.setText(selectedPodcast.getTitle()+" notes");
                     //TODO need to make listener to auto-save the text for the notes
                     noteArea.setText(selectedPodcast.getNoteArea().getText());
-//                    podcastCover.setImage(new Image(getClass().getResource("podcast.jpg").toExternalForm()));
+                    Image img = new Image(selectedPodcast.getImgPath());
+                    podcastCover.setImage(img);
+                    podcastCover.setPreserveRatio(true);
+                    podcastCover.setSmooth(true);
+                    podcastCover.setFitHeight(165);
+                    podcastCover.setFitWidth(200);
                 }));
         //Set default selection to first
         queueView.getSelectionModel().selectFirst();
@@ -139,7 +136,8 @@ public class Controller implements Initializable {
         Parent root;
         try {
             //Loads pop-up window using addWindow.fxml
-            root = FXMLLoader.load(new File(m.ADD_WINDOW).toURL());
+//            root = FXMLLoader.load(new File(m.ADD_WINDOW_PATH).toURL());
+            root = FXMLLoader.load(getClass().getResource(m.ADD_WINDOW_PATH));
             Stage stage = new Stage();
             stage.setTitle("Add new feed");
             stage.setScene(new Scene(root));
@@ -152,12 +150,14 @@ public class Controller implements Initializable {
             e.printStackTrace();
         }
         //Connected to the model to get the controller of the add window to get the string entered as the "rssfeed"
-        String rssFeed = m.getSecondaryController().getRssFeed();
+        String rssFeed = m.getAddWindow().getRssFeed();
+        queueView.getSelectionModel().selectFirst();
         System.out.println("The link entered in the secondary window was: "+rssFeed);
 
         //Will be null if addWindow is closed or cancelled
+        //TODO Is this needed?
         if(!rssFeed.isBlank()){
-            buildPodcast(rssFeed, m.getSecondaryController().getPodcastCount());
+            buildPodcast(rssFeed, m.getAddWindow().getPodcastCount());
         }
 
     }
@@ -165,8 +165,8 @@ public class Controller implements Initializable {
     public void libBtn(ActionEvent event){
         Parent root;
         try {
-            //Loads pop-up window using addWindow.fxml
-            root = FXMLLoader.load(new File(m.LIB_WINDOW).toURL());
+            //Loads library scene using library.fxml
+            root = FXMLLoader.load(getClass().getResource(m.LIB_WINDOW_PATH));
             Stage stage = new Stage();
             stage.setTitle("My Library");
             stage.setScene(new Scene(root));
@@ -184,15 +184,14 @@ public class Controller implements Initializable {
     public void toggleBtn(ActionEvent event){
         //TODO find why icons aren't loading
         //TODO make sure when a podcast is playing it is stopped before playing another one
-        javafx.scene.image.Image img = null;
-        if(isPlaying) {
+        Image img = null;
 
-//            img= new Image(getClass().getResource("playIcon.png").toExternalForm());
-            nowPlayingLabel.setText("");
+        if(isPlaying){
+            img = new Image("images/playIcon.png");
         } else {
-//            img = new Image(getClass().getResource("pause.png").toExternalForm());
-            nowPlayingLabel.setText("Now playing: "+selectedPodcast.getTitle());
+            img = new Image("images/pause.png");
         }
+
         isPlaying = !isPlaying;
         toggleBtnIcon.setImage(img);
 
