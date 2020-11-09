51e53aef37fef93f738715bbeec754dcf816398c
James Adams <jadams18@austincollege.edu>
2020-11-09 17:33:12 -0600

    Converted boolean properties to simpleBooleanProperties

--- src/Main/Podcast.java
+++ src/Main/Podcast.java
@@ -1,6 +1,8 @@
 package Main;
 
 import javafx.beans.Observable;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.util.Callback;
@@ -12,31 +14,33 @@ public class Podcast {
 
     private final StringProperty author = new SimpleStringProperty(this, "author", "");
     private final StringProperty title = new SimpleStringProperty(this, "title", "");
+    private final BooleanProperty isPlaying = new SimpleBooleanProperty(this, "isPlaying", false);
+    private final BooleanProperty hasNotes = new SimpleBooleanProperty(this, "hasNotes", false);
     private int  progress, maxProgress;
     private JTextArea noteArea;
-    private String imgPath, enclosurePath, duration;
-    private boolean isPlaying, hasNotes;
+    private String imgPath, enclosurePath, duration, pubDate;
 
     public Podcast(){
 
         this.progress = 0;
-        this.isPlaying = false;
-        this.hasNotes = false;
+        this.isPlaying.set(false);
+        this.hasNotes.set(false);
 
     }
 
-    public Podcast(String Title, String Author, String imgPath, Node enclosure, String duration){
+    public Podcast(String Title, String Author, String imgPath, Node enclosure, String duration, String pubDate){
 
         this.title.set(Title);
         this.author.set(Author);
         this.noteArea = new JTextArea();
         this.noteArea.setText("Write your first note here!");
         this.duration = duration;
+        this.pubDate = pubDate;
 
         this.imgPath = imgPath;
-        this.progress = 50;
-        this.isPlaying = false;
-        this.hasNotes = false;
+        this.progress = 0;
+        this.isPlaying.set(false);
+        this.hasNotes.set(false);
 
         this.enclosurePath = enclosure.getAttributes().getNamedItem("url").getTextContent();
         this.maxProgress = Integer.parseInt(enclosure.getAttributes().getNamedItem("length").getTextContent());
@@ -75,20 +79,23 @@ public class Podcast {
 
     public String getDuration() { return this.duration;}
 
+    public String getPubDate() { return this.pubDate; }
+
     public boolean isPlaying() {
-        return isPlaying;
+        return this.isPlaying.get();
     }
 
-    public void setPlaying(boolean playing) {
-        isPlaying = playing;
+    public void togglePlaying() {
+        boolean temp = isPlaying.get();
+        isPlaying.set(temp);
     }
 
     public boolean hasNotes(){
-        return noteArea.getText().isBlank();
+        return !noteArea.getText().equalsIgnoreCase("Write your first note here!");
     }
 
     public static Callback<Podcast, Observable[]> extractor = p -> new Observable[]
-            {p.title};
+            {p.title, p.isPlaying, p.hasNotes};
 
     public String dump(){
         return this.title.get()+" "+this.author.get()+" "+this.imgPath+" "+this.enclosurePath+" "+this.maxProgress;
