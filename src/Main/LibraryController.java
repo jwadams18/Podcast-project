10d7970709a5055d50fe4e714c37b896abb4fe9d
James Adams <jadams18@austincollege.edu>
2020-11-08 23:36:28 -0600

    Implemented basics for listView, that will have data loaded in

--- src/Main/LibraryController.java
+++ src/Main/LibraryController.java
@@ -1,5 +1,9 @@
 package Main;
 
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,16 +12,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LibraryController implements Initializable {
 
     private Model m = Main.model;
+    private final ObservableList<Podcast> libraryList = FXCollections.observableArrayList(Podcast.extractor);
+    private ChangeListener<Podcast> libraryChangeListener;
+    private Podcast selectedPodcast;
 
     @FXML
     private Button backBtn;
@@ -28,11 +37,46 @@ public class LibraryController implements Initializable {
     @FXML
     private ListView<Podcast> libraryView;
 
+    @FXML
+    private AnchorPane listCover;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Stores controller in model, so we can transfer data between if needed
+        m.setLibController(this);
+
+        //Sets the 'format' for listView
         libraryView.setCellFactory(new LibCellFactory());
-        libraryView.setItems(null);
+        libraryView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        //Fills with sample data from model
+//        Model.fillSampleData(libraryList);
+
+        //Sorts list based on comparator defined below
+        SortedList<Podcast> sortedList = new SortedList<>(libraryList);
+
+        sortedList.setComparator((Podcast1, Podcast2) -> {
+            int result = Podcast1.getAuthor().compareToIgnoreCase(Podcast2.getAuthor());
+
+            if(result == 0){
+                result = Podcast1.getTitle().compareToIgnoreCase(Podcast2.getTitle());
+            }
+            return result;
+        });
+
+        libraryView.setItems(sortedList);
+
+
+        if(libraryView.getItems().size() == 0){
+            listCover.setVisible(true);
+            addBtn.setVisible(false);
+        } else {
+            listCover.setVisible(false);
+            libraryView.getSelectionModel().selectFirst();
+        }
+
+
     }
 
     @FXML
