f48505c6140b2862881d07318bf3a4df36e2160d
James Adams <jadams18@austincollege.edu>
2020-12-03 00:00:29 -0600

    Added application icon

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("NoteCast!");
         primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("resources/musicnote.png")));
         primaryStage.show();
     }
 
